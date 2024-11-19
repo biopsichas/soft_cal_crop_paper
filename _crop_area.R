d3ff6fbf591fb3fec6942855e55304ab69d75b4a
biopsichas <svajunas.plunge@gmail.com>
2024-11-19 10:20:03 +0200

    confusion with area calculation

--- _crop_area.R
+++ _crop_area.R
@@ -1,48 +1,210 @@
 ##==============================================================================
-## 1) Get the crop area
+## 1) Loading shape files and extracting the data
 ##==============================================================================
 
-nb_harvest <- read.csv(paste("Data", "number_of_harvests.csv", sep = "/"))
+f_shp <- gsub(path_to_mod, path_shapes, folders)
+df <- NULL
+##Read all the shapefiles and extract the data
+for(f in f_shp){
+  # Read the shapefile
+  print(f)
+  shp <- read_sf(list.files(path = f, pattern = ".shp$", full.names = TRUE))
+  if(!"type" %in% names(shp)) shp$type <- shp$lu
+  shp$area <- as.vector(st_area(shp))
+  shp <- shp %>%
+    select(type, area, starts_with("y_")) %>%
+    st_drop_geometry
+  ##Removing "NA" from all columns if exists in shp dataframe
+  shp[shp == "NA"] <- NA
+  shp[] <- lapply(seq_len(ncol(shp)), function(i) {
+    if (i == which(names(shp) == "type")) {
+      return(shp[[i]])  # Keep the 'type' column unchanged
+    } else {
+      return(ifelse(is.na(shp[[i]]), shp$type, shp[[i]]))}})
+  shp <- shp %>%
+    pivot_longer(cols = -c(type, area), names_to = "year", values_to = "crop") %>%
+    mutate(year = as.numeric(gsub("[^0-9]", "", year))) %>%
+    select(-type) %>%
+    mutate(cs_name = basename(f))
+  if(is.null(df)) df <- shp else df <- bind_rows(df, shp)
+  print("done!")
+}
 
-crop_area <- read_yields(f_ini) %>%
-  select(plant_name, `harv_area(ha)`, cs_name) %>%
-  filter(`harv_area(ha)` > 0) %>%
-  left_join(nb_harvest, by = c("cs_name", "plant_name" = "crop"))%>%
-  mutate(harvest = ifelse(is.na(harvest),  1, harvest)) %>%
-  mutate(harv_area_ha = `harv_area(ha)` / harvest) %>%
-  left_join(lookup, by = "cs_name") %>%
-  select(plant_name, harv_area_ha, cs_label)
+## Cleaning crop labels
+df <- mutate(df, crop = str_replace_all(crop, "_.*$", "")) %>%
+  group_by(cs_name, crop, year) %>%
+  summarise(area = sum(area))
 
 ##==============================================================================
-## 2) Check the sums of the crop area for each case study
+## 2) Loading management files and extracting data
 ##==============================================================================
 
-crop_sums <- crop_area %>% group_by(cs_label) %>%
-  summarise(sum_harv_area_ha = round(sum(harv_area_ha)/100,0))
-crop_sums
+##Extract management data
+df_mgt <- NULL
+for(f in f_shp){
+  gen <- list.files(path = f, pattern = "generic", full.names = TRUE)
+  crp <- list.files(path = f, pattern = "mgt_crop", full.names = TRUE)
+  if(length(crp) == 0)  crp <- list.files(path = f, pattern = "crop", full.names = TRUE)
+  gen <- read.csv(gen) %>%
+    rename(lu = 1) %>%
+    mutate(cs_name = basename(f),
+           mgt_type = "generic",
+           op_data3 = as.character(op_data3))
+  crp <- read.csv(crp) %>%
+    rename(lu = 1) %>%
+    mutate(cs_name = basename(f),
+           mgt_type = "crop",
+           op_data3 = as.character(op_data3))
+  if(is.null(df_mgt)) df_mgt <- gen else df_mgt <- bind_rows(df_mgt, gen)
+  df_mgt <- bind_rows(df_mgt, crp)
+}
 
 ##==============================================================================
-## 3) Calculate the percentage of the crop area for each crop
+## 3) Cleaning management data
 ##==============================================================================
 
-crop_area_prec <- crop_area %>%
-  left_join(crop_sums, by = "cs_label") %>%
-  mutate(proc = harv_area_ha / sum_harv_area_ha)
+## Clean the management data
+df_mgt_link <- df_mgt %>%
+  #filter by operation column start with "harvest"
+  filter(str_detect(operation, '^harvest')|str_detect(operation, '^initial_plant')) %>%
+  select(lu, op_data1, cs_name) %>%
+  filter(!(grepl("^cov",lu) & grepl("^radi",op_data1))) %>% ##Removing cover crops with radishes
+  filter(!(grepl("CS7",cs_name) & grepl("^radi|^ryeg",op_data1))) %>% ##Removing cover crops with radishes))
+  filter(!(grepl("CS2",cs_name) & grepl("^shrb",op_data1))) %>%
+  filter(!(grepl("CS9",cs_name) & grepl("^mustard",op_data1))) %>%
+  mutate(lu = str_replace_all(lu, "_[0-9]+\\.[0-9]+yr", "")) %>% ##Removing the pastures with different harvests patterns
+  mutate(lu = str_replace_all(lu, "_.*$", "")) %>%
+  unique() %>%
+  group_by(cs_name, lu)%>%
+  slice(1) %>%
+  ungroup()
+
+df_mgt_link[df_mgt_link$cs_name %in% c("CS3", "CS11") & df_mgt_link$lu== "meadow", "lu"] <- "pasture"
 
 ##==============================================================================
-## 4) Plot the treemap
+## 4) Get the number of years for each case study
+##==============================================================================
+df_years <- df %>%
+  ungroup() %>%
+  select(cs_name, year) %>%
+  group_by(cs_name) %>%
+  summarise(year_min = min(year), year_max = max(year)) %>%
+  mutate(years = year_max - year_min + 1) %>%
+  select(cs_name, years) %>%
+  left_join(lookup, by = "cs_name")
+
+##==============================================================================
+## 5) Testing are the areas right
+##==============================================================================
+df %>% ungroup %>%
+  select(-crop) %>%
+  group_by(cs_name) %>%
+  summarise(area = round(sum(area)/1000000, 2)) %>%
+  left_join(df_years, by = "cs_name") %>%
+  mutate(area = area/years)
+
+##==============================================================================
+## 6) Clean the crop rotation data and join with the management data (to get crop codes)
 ##==============================================================================
 
+df_clean <- df %>%
+  select(-year) %>%
+  group_by(cs_name, crop) %>%
+  summarise(area = sum(area)) %>%
+  ungroup() %>%
+  left_join(df_mgt_link, by = c("crop" = "lu", "cs_name")) %>%
+  mutate(op_data1 = str_replace_all(op_data1, "_.*$", "")) %>%
+  mutate(op_data1= str_replace_all(op_data1, "[0-9]", "")) %>%
+  filter(!is.na(op_data1) & !op_data1 %in% c("rnge", "frst", "rnge_test", "orcd", "rngb",
+                                             "wetl", "frst_test", "wetn", "frst_comm",
+                                             "orcd_comm", "wetl_comm", "frsd", "frse", "oak",
+                                             "pine", "popl", "will", "frsdit", "hayd")) %>%
+  left_join(lookup, by = "cs_name")
+
+
+##==============================================================================
+## 7) Testing are agricultral areas right
+##==============================================================================
+
+df_clean %>%
+  ungroup() %>%
+  select(-op_data1) %>%
+  group_by(cs_name, cs_label) %>%
+  summarise(area = sum(area)) %>%
+  ungroup() %>%
+  left_join(df_years, by = "cs_name") %>%
+  mutate(area = round(area/years/1000000, 2))
+
+## Check if there are any missing or not needed crop codes
+unique(df_clean$op_data1)
+
+##==============================================================================
+## 8) Calculate the percentage of each crop in the rotation
+##==============================================================================
+
+df_final <- left_join(df_clean, select(df_clean, cs_name, area) %>%
+                        group_by(cs_name) %>%
+                        summarise_all(sum) %>%
+                        rename(area_sum = area), by = "cs_name") %>%
+  mutate(proc = round(100*area/area_sum, 1)) %>%
+  select(-starts_with(c("area", "crop"))) %>%
+  group_by(cs_label, op_data1) %>%
+  summarise(proc = sum(proc)) %>%
+  ungroup() %>%
+  rename(crop = op_data1)
+
+df_final_area <- df_clean %>%
+  select(-crop) %>%
+  group_by(cs_label, op_data1) %>%
+  summarise(area = sum(area)) %>%
+  left_join(select(df_clean, cs_label, area) %>%
+              group_by(cs_label) %>%
+              summarise_all(sum) %>%
+              rename(area_sum = area), by = "cs_label") %>%
+  left_join(df_years, by = "cs_label") %>%
+  mutate(area = round(0.000001* area / years, 2),
+         area_sum = round(0.000001* area_sum / years, 2)) %>%
+  select(-years)
+
+##==============================================================================
+## 9) Check the sums
+##==============================================================================
+
+df_final[c("cs_label","proc")] %>% group_by(cs_label) %>% summarise_all(sum)
+
+##==============================================================================
+## 10) Prepare excel tables
+##==============================================================================
+table <- df_final %>%
+  pivot_wider(names_from = cs_label, values_from = proc)
+order_cs <- order_cs[order_cs != lookup[!lookup$cs_name %in% unique(res$cs), "cs_label"]]
+table <- table[, c("crop", order_cs)]
+
+##==============================================================================
+## 11) Plot the data
+##==============================================================================
 ## Order of the case studies in the plots
-crop_area_prec$cs_label  <- factor(crop_area_prec$cs_label, levels = order_cs, ordered = TRUE)
+df_final$cs_label  <- factor(df_final$cs_label, levels = order_cs, ordered = TRUE)
 
+## Plot the area data
+fig6 <- ggplot(df_final, aes(reorder_within(crop, proc, within= tolower(cs_label), sep = ">"), proc)) +
+  geom_bar(stat="identity",fill = "blue") +  # Adjust binwidth as needed
+  facet_wrap(~cs_label, scales = "free") +
+  labs(title = "Precentage of crop areas during all rotations",
+       x = "Codes",
+       y = "% by area") +
+  theme_minimal()+
+  theme(strip.text.x = element_text(face = "bold", size = 12))+
+  coord_flip()
+
+## Plot the data in a treemap
 ## Prepare color schema
-n <- length(unique(crop_area_prec$plant_name))
+n <- length(unique(df_final$crop))
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 color_schema <-  unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
 ## Plot the treemap
-fig5 <- ggplot(crop_area_prec, aes(area = proc, fill = plant_name, label =  plant_name)) +
+fig7 <- ggplot(df_final, aes(area = proc, fill = crop, label = crop)) +
   geom_treemap() +
   geom_treemap_text(colour = "black",
                     place = "centre",
@@ -57,21 +219,19 @@ fig5 <- ggplot(crop_area_prec, aes(area = proc, fill = plant_name, label =  plan
         legend.position="none",
         plot.margin = unit(c(0,5,5,37), "points"))
 
-fig5
 
 ##==============================================================================
-## 5) Save the plots
+## 12) Save the plots
 ##==============================================================================
 
 fig_path <- paste(results_folder, "figs", sep = "/")
 if (!file.exists(fig_path)) dir.create(fig_path)
 
-ggsave(paste(fig_path, "fg5_crop_areas.png", sep = "/"), plot = fig5,
+ggsave(paste(fig_path, "fg6_crop_areas_km2_pr.jpeg", sep = "/"), plot = fig6,
        width = 1000, height = 700, units = "px", dpi = 100)
 
-table <- crop_area_prec %>%
-  select(-c(sum_harv_area_ha, proc)) %>%
-  mutate(harv_area_ha = round(harv_area_ha/100, 1)) %>%
-  pivot_wider(names_from = cs_label, values_from = harv_area_ha)
+ggsave(paste(fig_path, "fg7_crop_areas_pr.png", sep = "/"), plot = fig7,
+       width = 1000, height = 700, units = "px", dpi = 100)
 
-openxlsx::write.xlsx(table, paste(fig_path, "areas.xlsx", sep = "/"),  rowNames = F)
+openxlsx::write.xlsx(table, paste(fig_path, "areas_previous.xlsx", sep = "/"),  rowNames = F)
+openxlsx::write.xlsx(df_final_area, paste(fig_path, "areas_km2.xlsx", sep = "/"),  rowNames = F)
